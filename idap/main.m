3009f9a52429c2ff66529957e66b7868b055edbb
Aleksey Zuiev <aleksei.zuew@gmail.com>
2017-04-20 18:41:27 +0300

    Initial commit -> create main

---
+++ idap/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  idap
+//
+//  Created by Aleksey Zuiev on 20/04/2017.
+//  Copyright © 2017 Aleksey Zuiev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
